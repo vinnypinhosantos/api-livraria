3b6cbdc529fe5d8ef78c2bbc68f9c09b828b88f2
vinnypinhosantos <vinnypinhosantos@gmail.com>
2023-01-11 12:34:18 -0300

    Associating Autores with Livros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,9 +3,11 @@ import livros from "../models/Livro.js"
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros)
-        })
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                res.status(200).json(livros)
+            })
     }
 
     static listarLivroPorID = (req, res) => {
@@ -25,7 +27,7 @@ class LivroController {
 
         livro.save((err) => {
             if (err) {
-                res.status(500).send({message: `${error.message} - erro ao cadastrar novo livro`})
+                res.status(500).send({message: `${err.message} - erro ao cadastrar novo livro`})
             } else {
                 res.status(201).send(livro.toJSON())
             }
